d9f3e781587522616b586bbe61afbb50a66ec61e
petr-pechkurov <petr.pechkurov@gmail.com>
2022-05-01 11:57:36 +0300

    JWT token for login implemented

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,15 +8,17 @@ import 'reflect-metadata';
 import IUserController from './users.controller.interface';
 import UserLoginDto from './dto/user-login.dto';
 import UserRegisterDto from './dto/user-register.dto';
-import User from './user.entity';
-import UserService from './user.service';
 import ValidateMiddleware from '../common/validate.middleware';
+import { sign } from 'jsonwebtoken';
+import IUserService from './user.service.interface';
+import IConfigService from '../config/config.service.interface';
 
 @injectable()
 export default class UserController extends BaseController implements IUserController {
 	constructor(
 		@inject(TYPES.ILogger) logger: ILogger,
-		@inject(TYPES.UserService) private userService: UserService,
+		@inject(TYPES.UserService) private userService: IUserService,
+		@inject(TYPES.ConfigService) private configService: IConfigService,
 	) {
 		super(logger);
 		this.bindRoutes([
@@ -44,7 +46,9 @@ export default class UserController extends BaseController implements IUserContr
 		if (!result) {
 			return next(new HTTPError(401, 'auth error', 'login'));
 		}
-		this.ok(res, { message: 'login successful' });
+
+		const jwt = await this.signJWT(body.email, this.configService.get('SECRET'));
+		this.ok(res, { jwt });
 	}
 
 	async register(
@@ -57,5 +61,26 @@ export default class UserController extends BaseController implements IUserContr
 			return next(new HTTPError(422, 'This user is already exists'));
 		}
 		this.ok(res, { email: result.email, id: result.id });
+	}
+
+	private signJWT(email: string, secret: string): Promise<string> {
+		return new Promise((resolve, reject) => {
+			sign(
+				{
+					email,
+					iat: Math.floor(Date.now() / 1000),
+				},
+				secret,
+				{
+					algorithm: 'HS256',
+				},
+				(err, token) => {
+					if (err) {
+						reject(err);
+					}
+					resolve(token as string);
+				},
+			);
+		});
 	}
 }
