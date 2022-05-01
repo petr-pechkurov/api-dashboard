cf41d6e7ecbe4fa09b911350ede5787b2fba6ed2
petr-pechkurov <petr.pechkurov@gmail.com>
2022-05-01 13:10:25 +0300

    auth middleware implemented

---
+++ src/common/auth.middleware.ts
@@ -0,0 +1,22 @@
+import IMiddleware from './middleware.interface';
+import { NextFunction, Request, Response } from 'express';
+import { JwtPayload, verify } from 'jsonwebtoken';
+
+export class AuthMiddleware implements IMiddleware {
+	constructor(private secret: string) {}
+
+	execute(req: Request, res: Response, next: NextFunction): void {
+		if (req.headers.authorization) {
+			verify(req.headers.authorization.split(' ')[1], this.secret, (err, payload) => {
+				if (err) {
+					next();
+				}
+				const data = payload as JwtPayload;
+				req.user = data.email;
+				next();
+			});
+		} else {
+			next();
+		}
+	}
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -34,6 +34,12 @@ export default class UserController extends BaseController implements IUserContr
 				func: this.register,
 				middlewares: [new ValidateMiddleware(UserRegisterDto)],
 			},
+			{
+				path: '/info',
+				method: 'get',
+				func: this.info,
+				middlewares: [],
+			},
 		]);
 	}
 
@@ -61,6 +67,10 @@ export default class UserController extends BaseController implements IUserContr
 			return next(new HTTPError(422, 'This user is already exists'));
 		}
 		this.ok(res, { email: result.email, id: result.id });
+	}
+
+	async info({ user }: Request, res: Response, next: NextFunction): Promise<void> {
+		this.ok(res, { email: user });
 	}
 
 	private signJWT(email: string, secret: string): Promise<string> {
